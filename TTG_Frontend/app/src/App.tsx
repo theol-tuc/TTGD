a8aba064db7f3a729162198ff4250cb94ca6b3a1
theol <t.lesser@tu-clausthal.de>
2025-04-09 11:38:06 +0300

    tsconfig and app update

--- TTG_Frontend/app/src/App.tsx
+++ TTG_Frontend/app/src/App.tsx
@@ -1,16 +1,18 @@
 import React, {JSX} from 'react';
 import './App.css';
+import { Board } from './board/board';
 
 function App(): JSX.Element {
-  return (
-      <div className="App">
-        <header className="App-header">
-            <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-        </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to Turing Tumble</h1>
+            </header>
+            <main>
+                <Board />
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- TTG_Frontend/app/src/board/board.tsx
+++ TTG_Frontend/app/src/board/board.tsx
@@ -1,1 +1,9 @@
-//put the board and its functions here
+import React from 'react';
+
+export const Board: React.FC = () => {
+    return (
+        <div className="board">
+            <p>Board here!</p>
+        </div>
+    );
+};
