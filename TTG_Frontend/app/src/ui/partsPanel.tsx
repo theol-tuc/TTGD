46a0c2e5e57a30a308cbb9a16dbff024b6e5dbc5
theol <t.lesser@tu-clausthal.de>
2025-04-09 19:38:44 +0300

    part pictures

--- TTG_Frontend/app/src/ui/partsPanel.tsx
+++ TTG_Frontend/app/src/ui/partsPanel.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Card, Space, Typography } from 'antd';
-import { ItemType } from '../parts/constants';
+import { ItemType, IMAGE_FILENAMES } from '../parts/constants';
 
 const { Title } = Typography;
 
 const partItems = [
+    { type: ItemType.BallBlue, name: 'Blue Ball' },
+    { type: ItemType.BallRed, name: 'Red Ball' },
     { type: ItemType.RampLeft, name: 'Ramp Left' },
     { type: ItemType.RampRight, name: 'Ramp Right' },
     //{ type: ItemType.Gear, name: 'Gear' },
     //{ type: ItemType.GearBit, name: 'Gear Bit' },
-    //{ type: ItemType.Interceptor, name: 'Interceptor' },
+    { type: ItemType.Intercept, name: 'Interceptor' },
     { type: ItemType.BitLeft, name: 'Bit Left' },
     { type: ItemType.BitRight, name: 'Bit Right' },
     //{ type: ItemType.Gearbox, name: 'Gearbox' },
@@ -40,13 +42,15 @@ export const PartsPanel: React.FC = () => {
                                 borderRadius: '6px',
                                 overflow: "hidden"}}
                             onClick={() => handlePartClick(part.type)}>
-                            <div style={{
-                                width: '10vw',
-                                height: '10vh',
-                                backgroundColor: '#f0f0f0',
-                                borderRadius: '4px',
-                                marginBottom: '8px'}} />
-                            <span style={{ fontSize: '11px' }}>{part.name}</span>
+                            <img
+                                src={IMAGE_FILENAMES[part.type]}
+                                alt={part.name}
+                                style={{
+                                    width: '40px',
+                                    height: '40px',
+                                    objectFit: 'contain',
+                                    marginBottom: '8px'}}/>
+                            <span style={{fontSize: '11px'}}>{part.name}</span>
                         </Card.Grid>
                     ))}
                 </Space>
